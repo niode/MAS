a20414fab15076433565e7bd2ebae6276afb5f77
Nathan Harms <nathanharms@gmail.com>
2014-02-25 10:17:04 -0700

    Added example pair to BasicData.

--- Agent/Agent/SAP/BasicData.java
+++ Agent/Agent/SAP/BasicData.java
@@ -1,10 +1,12 @@
 package Agent.SAP;
 
+import Agent.SAP.Actions.*;
+
 public class BasicData extends Data
 {
   public BasicData()
   {
     super();
-    list.add(new Pair(null, null));
+    list.add(new Pair(new Coordinate(new long[]{0,0}), new SleepAction()));
   }
 }
