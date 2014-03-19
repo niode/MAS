c62e8f3d10637514a06a33102856a43712891c07
DanWehr <danwehr@gmail.com>
2014-03-19 01:39:35 -0600

    Added role files for Team, Charging and Observer roles.

---
+++ Agent/Agent/Role/ObserverRole.java
@@ -0,0 +1,46 @@
+package Agent.Role;
+
+import java.util.ArrayList;
+import Agent.Communicator;
+import Agent.Simulation;
+import Agent.Core.BaseAgent;
+import Agent.Role.Rules.Rule;
+
+/**
+ *
+ */
+public class ObserverRole extends Role
+	{
+
+	/**
+	 * @param sim
+	 * @param com
+	 * @param base
+	 */
+	public ObserverRole(Simulation sim, Communicator com, BaseAgent base)
+		{
+		super(sim, com, base);
+		// TODO Auto-generated constructor stub
+		}
+
+	/* (non-Javadoc)
+	 * @see Agent.Role.Role#setupRules(java.util.ArrayList)
+	 */
+	@Override
+	public void setupRules(ArrayList<Rule> rules)
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	/* (non-Javadoc)
+	 * @see Agent.Role.Role#noRuleMatch()
+	 */
+	@Override
+	public void noRuleMatch()
+		{
+		// TODO Auto-generated method stub
+
+		}
+
+	}
