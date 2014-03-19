b180bb1f0df51ba8069cfce0d4b88cb01a68b07e
DanWehr <danwehr@gmail.com>
2014-03-19 11:10:01 -0600

    Two example rules now up. You can see how they're used in the Explorer role.

--- Agent/Agent/Role/ExplorerRole.java
+++ Agent/Agent/Role/ExplorerRole.java
@@ -7,20 +7,11 @@ import java.util.ArrayList;
 import Agent.Communicator;
 import Agent.Simulation;
 import Agent.Core.BaseAgent;
-import Agent.Pathfinder.Path;
-import Agent.Pathfinder.PathOptions;
-import Agent.Pathfinder.Pathfinder;
-import Agent.Role.Rules.Rule;
-import Ares.Direction;
+import Agent.Pathfinder.*;
+import Agent.Role.Rules.*;
 import Ares.Location;
 import Ares.Commands.AgentCommand;
 import Ares.Commands.AgentCommands.MOVE;
-import Ares.Commands.AgentCommands.SAVE_SURV;
-import Ares.Commands.AgentCommands.TEAM_DIG;
-import Ares.World.Cell;
-import Ares.World.Objects.Rubble;
-import Ares.World.Objects.Survivor;
-import Ares.World.Objects.WorldObject;
 
 /**
  * Basic role for exploration. The agent will not cooperate, but encountering situations that
@@ -48,49 +39,25 @@ public class ExplorerRole extends Role
 	@Override
 	public void setupRules(ArrayList<Rule> rules)
 		{
-		// TODO Auto-generated method stub
+		rules.add(new RuleSaveSurvivor());
+		rules.add(new RuleCanDig());
 		}
 
 	@Override
 	public void noRuleMatch()
 		{
 		Location currentLoc = getSimulation().getSelf().getLocation();
-		Cell currentCell = getSimulation().getCell(currentLoc);
-		WorldObject topLayer = currentCell.getTopLayer();
-		
-		//If on a survivor, save them.
-		if (topLayer instanceof Survivor)
-			{
-			getCommunicator().send(new SAVE_SURV());
-			return;
-			}
-		
-		//If already on rubble pile or with an agent, dig.
-		if (topLayer instanceof Rubble)
-			{
-			Rubble rubble = (Rubble)topLayer;
-			
-			if (rubble.getRemoveAgents() == 1)
-				{
-				//Dig if only one agent needed.
-				getCommunicator().send(new TEAM_DIG());
-				return;
-				}
-			else
-				{
-				//Rubble must require 2 agents, dig if another there.
-				if (currentCell.getCellInfo().getAgentIDList().size() > 1)
-					{
-					getCommunicator().send(new TEAM_DIG());
-					return;
-					}
-				}
-			}
 		
 		//Get path to nearest survivor.
 		//We could save this, but we'll recalculate each turn.
 		PathOptions opt = new PathOptions(currentLoc);
 		Path nearestSurvPath = Pathfinder.getNearestSurvivor(getSimulation(), opt);
+		
+		/*
+		 * TODO this crashes right now because the nearest survivor will be where the agent is already
+		 * standing (just under rubble), and so the path is empty. Will make this something better
+		 * later today.
+		 */
 		
 		//Get next location in path and move to it.
 		Location moveTo = nearestSurvPath.getNext();
