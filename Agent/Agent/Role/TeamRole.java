f3388fcd56761c427f5979cd5f8c1a40050f83f0
Nathan Harms <nathanharms@gmail.com>
2014-03-20 11:37:49 -0600

    Removed auto-generated whatevers.

--- Agent/Agent/Role/TeamRole.java
+++ Agent/Agent/Role/TeamRole.java
@@ -10,37 +10,36 @@ import Agent.Role.Rules.Rule;
  *
  */
 public class TeamRole extends Role
-	{
+{
 
-	/**
-	 * @param sim
-	 * @param com
-	 * @param base
-	 */
-	public TeamRole(Simulation sim, Communicator com, BaseAgent base)
-		{
-		super(sim, com, base);
-		// TODO Auto-generated constructor stub
-		}
+  /**
+   * @param sim
+   * @param com
+   * @param base
+   */
+  public TeamRole(Simulation sim, Communicator com, BaseAgent base)
+  {
+    super(sim, com, base);
+    
+  }
 
-	/* (non-Javadoc)
-	 * @see Agent.Role.Role#setupRules(java.util.ArrayList)
-	 */
-	@Override
-	public void setupRules(ArrayList<Rule> rules)
-		{
-		// TODO Auto-generated method stub
+  /* (non-Javadoc)
+   * @see Agent.Role.Role#setupRules(java.util.ArrayList)
+   */
+  @Override
+  public void setupRules(ArrayList<Rule> rules)
+  {
+    
 
-		}
+  }
 
-	/* (non-Javadoc)
-	 * @see Agent.Role.Role#noRuleMatch()
-	 */
-	@Override
-	public void noRuleMatch()
-		{
-		// TODO Auto-generated method stub
+  /* (non-Javadoc)
+   * @see Agent.Role.Role#noRuleMatch()
+   */
+  @Override
+  public void noRuleMatch()
+  {
 
-		}
+  }
 
-	}
+  }
