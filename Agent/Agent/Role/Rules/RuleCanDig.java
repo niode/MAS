cf3b1063d4909af21b0a2b2cfa89e40dc9895a70
DanWehr <danwehr@gmail.com>
2014-03-27 22:50:17 -0600

    RuleCanDig now more intelligent. Should not result in stacking Explorers.
    Fixed runServer.bat to point to new world location.

--- Agent/Agent/Role/Rules/RuleCanDig.java
+++ Agent/Agent/Role/Rules/RuleCanDig.java
@@ -1,9 +1,12 @@
 package Agent.Role.Rules;
 
+import java.util.LinkedList;
+import java.util.List;
 import Agent.Communicator;
 import Agent.Simulation;
 import Agent.Core.BaseAgent;
 import Agent.Role.Role;
+import Ares.AgentID;
 import Ares.Location;
 import Ares.Commands.AgentCommand;
 import Ares.Commands.AgentCommands.TEAM_DIG;
@@ -29,18 +32,66 @@ public class RuleCanDig implements Rule
 		Location currentLoc = sim.getAgentLocation(sim.getSelfID());
 		Cell currentCell = sim.getCell(currentLoc);
 		WorldObject topLayer = currentCell.getTopLayer();
+		int selfIDint = sim.getSelfID().getID();
 		
+		//If there are 2+ team agents on this cell, don't dig.
+		List<AgentID> agentsHere = sim.getAgentsAt(currentLoc);
+		int teamCount = 0;
+		for (AgentID id : agentsHere)
+			if (sim.getAgentRole(id) == Role.ID.TEAM)
+				teamCount++;
+		if (teamCount >= 2)
+			return false;
+
 		if (topLayer instanceof Rubble && sim.getPercentage(currentLoc) > 0)
 			{
-			Rubble rubble = (Rubble)topLayer;
+			Rubble rubble = (Rubble) topLayer;
+			//Get list of Explorer agents on this tile.
+			LinkedList<AgentID> explorersHere = new LinkedList<AgentID>();
+			for (AgentID id : agentsHere)
+				if (sim.getAgentRole(id) == Role.ID.EXPLORER)
+					explorersHere.add(id);
 			
 			if (rubble.getRemoveAgents() == 1)
-				return true;
-			
-      if(sim.getAgentsAt(currentLoc).size() > 1)
-        return true;
+				{
+				//Dig if the only explorer here.
+				if (explorersHere.size() == 1)
+					return true;
+				
+				//2+ explorers. Dig if the lowest ID explorer.
+				int lowestID = Integer.MAX_VALUE;
+				for (AgentID id : explorersHere)
+					if (id.getID() < lowestID)
+						lowestID = id.getID();
+				if (selfIDint == lowestID)
+					return true;
+				}
+			else
+				{
+				//Must require two agents to do.
+				//Dig if only one other explorer here.
+				if (explorersHere.size() == 2)
+					return true;
+				
+				//More than two explorers, dig if one of the lowest ids.
+				int first = Integer.MAX_VALUE;
+				int second = Integer.MAX_VALUE;
+				for (AgentID id : explorersHere)
+					{
+					int test = id.getID();
+					if (test < first)
+						{
+						second = first;
+						first = test;
+						}
+					else if (test < second)
+						second = test;
+					}
+				if (selfIDint == first || selfIDint == second)
+					return true;
+				}
 			}
-		
+
 		return false;
 		}
 
