9412aa557b7172af7b20359f9a89db4e9eb58d9d
Aragna <mine.doror@club.fr>
2014-04-03 15:13:34 +0000

    add place dig beacon in charging rule need to modify this rule so as to make difference between several agent needed and only one

--- Agent/Agent/Role/ChargingRole.java
+++ Agent/Agent/Role/ChargingRole.java
@@ -41,6 +41,7 @@ public class ChargingRole extends Role
 	@Override
 	public void setupRules(ArrayList<Rule> rules)
 		{
+		rules.add(new RulePlaceDigBeacon());
 		rules.add(new RuleChargeOnLowEnergy());
 		rules.add(new RuleExploreIfAlone());
 		rules.add(new RuleWaitForAnother());
